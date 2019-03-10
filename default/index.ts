af2ca1814052213075003464732ba73c7d93359c
Mykhailo Stadnyk <mikhus@gmail.com>
2019-03-09 17:59:05 -0800

    allow run or import; + tslint

--- default/index.ts
+++ default/index.ts
@@ -2,7 +2,9 @@
 import { serviceOptions } from './config';
 import { %SERVICE_CLASS_NAME } from './src';
 
-(async () => {
+export * from './src';
+
+!module.parent && (async () => {
     const service = new %SERVICE_CLASS_NAME(serviceOptions);
     await service.start();
 })();
